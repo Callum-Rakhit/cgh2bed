90b8877bdd8b575f59aeacfe452ca489f2f462f3
Callum-Rakhit <callum_rakhit@hotmail.com>
2019-01-31 11:36:59 +0000

    Changed to a two column genomecov file

--- main.sh
+++ main.sh
@@ -1,23 +1,33 @@
 #!/usr/bin/env sh
 
-
 # Covert files to bed format and summarise all chromosomes in one file
+# Saves output as "summary.bed"
 for file in *.txt; do Rscript ./script_1.R $file; done
 
-#Sort the bed file
+# Sort the bed file
 # bedtools sort -chrThenSizeA -i summary.bed > summary_sorted.bed
 sort-bed summary.bed > summary_sorted.bed
 
-# Annotate each base depending with features
-bedtools genomecov -d -i summary.txt -g hg19.txt > summary_per_base.txt
+# Create a merged genome file of the key regions of interest
+bedtools merge -i summary.bed -d 5 > summary_merged
 
-# Filter out bases that don't have more than 100 features
-# local var1='(NR>1) && ($3>50)'  # No spaces allowed in variable declaration (=)
-# local summary_over_10 = awk $var1 summary_per_base.txt
-# awk '(NR>1) && ($3>10)' summary_per_base.txt > summary_over_10.txt
+# Take only chromosome number and the end position
+awk -v OFS='\t' '{print $1, $3}' summary_merged.bed > summary_merged_genome.bed
+
+# Annotate each base depending with features
+# This works by looking at the chrosome number and the crawling through each base up to the number
+# in the first column, e.g. chr1 100 would be 1:100 for chr1. Must be tab deliminated.
+
+# -dz only non-zero coverage reported
+# -bg report in bedgraph format
+
+bedtools genomecov -d -i summary_sorted.bed -g summary_merged_genome.bed > summary_per_base.txt
+
+# Select bases with than 0 features (can change this to whatever cutoff needed)
+awk '(NR>1) && ($3>0)' summary_per_base.txt > summary_non_zero.txt
   
-# Covert output back to bed file
+# Covert output back to bed file using an R script
 Rscript ./script_2.R summary_over_10.txt > summary_over_10.bed
   
-# Merge overlapping regions, combine regions that overlap (-d) by 5
-bedtools merge -i summary_over_50.bed -d 5 > merged_50.bed
+# Merge overlapping regions, combine regions that overlap (-d) by 2
+bedtools merge -i summary_over_50.bed -d 2 > merged_50.bed
